9f3c29841f83c077ed646310b4450b3cd9421609
sava <savaneli.david@gmail.com>
2024-02-23 02:54:20 -0800

    change fonts

--- src/views/home/index.tsx
+++ src/views/home/index.tsx
@@ -73,9 +73,22 @@ const View = () => {
       </MagneticLayout>
       <br />
       <br />
-      <div style={{fontSize: '20px'}}>
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. In blanditiis aut quibusdam? Recusandae rem voluptatum
-      earum inventore cupiditate in ducimus iusto voluptatibus, sunt soluta magni aliquid iure modi totam explicabo?</div>
+      <div className='f-regular' style={{ fontSize: '20px', margin: '50px 0' }}>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. In blanditiis aut quibusdam? Recusandae rem voluptatum
+        earum inventore cupiditate in ducimus iusto voluptatibus, sunt soluta magni aliquid iure modi totam explicabo?
+      </div>
+      <div className='f-medium' style={{ fontSize: '20px', margin: '50px 0' }}>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. In blanditiis aut quibusdam? Recusandae rem voluptatum
+        earum inventore cupiditate in ducimus iusto voluptatibus, sunt soluta magni aliquid iure modi totam explicabo?
+      </div>
+      <div className='f-semiBold' style={{ fontSize: '20px', margin: '50px 0' }}>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. In blanditiis aut quibusdam? Recusandae rem voluptatum
+        earum inventore cupiditate in ducimus iusto voluptatibus, sunt soluta magni aliquid iure modi totam explicabo?
+      </div>
+      <div className='f-bold' style={{ fontSize: '20px', margin: '50px 0' }}>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. In blanditiis aut quibusdam? Recusandae rem voluptatum
+        earum inventore cupiditate in ducimus iusto voluptatibus, sunt soluta magni aliquid iure modi totam explicabo?
+      </div>
       <br />
       <br />
       <div style={{ height: '300px', border: '1px solid red', backgroundColor: 'transparent' }}></div>
