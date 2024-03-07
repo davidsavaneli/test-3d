3033296a0527f2370c0e9d1a1a34258ce4286c9f
sava <savaneli.david@gmail.com>
2024-03-07 01:49:33 -0800

    change logo

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,6 +1,8 @@
 import React, { memo } from 'react'
 import { motion } from 'framer-motion'
-import { ImageSvg, MagneticLayout } from 'components'
+import { animateScroll as Scroll } from 'react-scroll'
+import { ImageSvg } from 'components'
+import { useCursorContext } from 'contexts'
 import animations from './animations'
 
 import styles from './styles.module.css'
@@ -8,12 +10,26 @@ import styles from './styles.module.css'
 import logo from 'assets/images/logo.svg'
 
 const Header = () => {
+  const { setCursorStyle } = useCursorContext()
+
+  const cursorProps = {
+    onMouseOver: () => setCursorStyle('default'),
+    onMouseOut: () => setCursorStyle('none'),
+  }
+
+  const scrollToTop = () => {
+    Scroll.scrollTo(0, {
+      duration: 1000,
+      smooth: 'easeOutQuad',
+    })
+  }
+
   return (
     <header className={styles.header}>
       <div className='container'>
         <div className='row'>
           <div className='col-6 d-flex align-items-center'>
-            <div className={styles.logoWrapper}>
+            <div className={styles.logoWrapper} {...cursorProps} onClick={() => scrollToTop()}>
               <motion.div
                 className={styles.logo}
                 variants={animations.logoVariant}
