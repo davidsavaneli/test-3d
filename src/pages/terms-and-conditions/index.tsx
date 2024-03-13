03c5aaca5da30ca93aed3e5e4617489f66cb1da9
sava <savaneli.david@gmail.com>
2024-03-13 03:21:07 -0700

    adds terms and conditions page

---
+++ src/pages/terms-and-conditions/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import type { NextPage } from 'next'
+import { AnimationLayout } from 'components'
+import View from 'views/terms-and-conditions'
+
+const TermsAndConditionsPage: NextPage = () => {
+  return (
+    <AnimationLayout>
+      <View />
+    </AnimationLayout>
+  )
+}
+
+export default TermsAndConditionsPage
--- src/views/privacy-policy/index.tsx
+++ src/views/privacy-policy/index.tsx
@@ -1,14 +1,12 @@
 import React from 'react'
 import { ContentPageLayout, AnimatedTitle } from 'components'
 
-import styles from './styles.module.css'
-
 const View = () => {
   return (
     <div className='container'>
       <ContentPageLayout
         title={
-          <AnimatedTitle transformDirection='ltr' size='medium' autoWidth>
+          <AnimatedTitle size='medium' autoWidth disableX>
             Privacy Policy
           </AnimatedTitle>
         }
