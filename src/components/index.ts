b4f94d77a5c24b8d04e0c292e10c2474a2c49bbb
sava <savaneli.david@gmail.com>
2024-03-21 15:57:52 +0400

    test

--- src/components/index.ts
+++ src/components/index.ts
@@ -50,4 +50,4 @@ export { default as Services } from './sections/Services'
 
 export { default as Projects } from './sections/Projects'
 
-export { default as Team } from './sections/Team'
+// export { default as Team } from './sections/Team'
